2d00ff90a633e843afccbc9e4cf03bb9d1969d94
christofs <c.schoech@gmail.com>
2016-03-22 23:56:28 +0100

    adding requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup
 
 setup(name="tmw",
-      version="0.2.1",
-      description="Topic Modeling Workflow",
-      url="http://github.com/cligs/tmw",
-      author="Christof Schöch",
-      author_email="c.schoech@gmail.com",
-      license="MIT",
-      packages=[""],
-      zip_safe=False)
+    version="0.2.1",
+    description="Topic Modeling Workflow",
+    url="http://github.com/cligs/tmw",
+    author="Christof Schöch",
+    author_email="c.schoech@gmail.com",
+    license="MIT",
+    packages=["tmw"],
+    install_requires=[
+    "pandas>==0.17.0",
+    "numpy>==1.9.0A",
+    ]
+    zip_safe=False)

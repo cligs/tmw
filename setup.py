cc1ab7efd9947f0c4824b7075fc51bc4358b1979
christofs <c.schoech@gmail.com>
2016-03-22 23:45:58 +0100

    Steps towards tmw package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="tmw",
+      version="0.2.1",
+      description="Topic Modeling Workflow",
+      url="http://github.com/cligs/tmw",
+      author="Christof Schöch",
+      author_email="c.schoech@gmail.com",
+      license="MIT",
+      packages=[""],
+      zip_safe=False)
--- tmw_activate.py
+++ tmw_activate.py
@@ -11,17 +11,11 @@
 import sys
 import os
 
-## Enter the path to the folder in which your toolbox folder is located.
-## Example: sys.path.append(os.path.abspath("/home/christof/Repos/cligs/"))
-sys.path.append(os.path.abspath("/home/christof/Repos/cligs/"))
-#sys.path.append(os.path.abspath("/usr/local/lib/python3.4/dist-packages/"))
-#sys.path.append(os.path.abspath("/usr/local/lib/python3.4/dist-packages/numpy-1.10.4.egg"))
+# Enter the path to the folder in which your toolbox folder is located.
+sys.path.append(os.path.abspath("/home/christof/Repos/cligs/tmw/"))
 
-## Optional: Activate to remove a (mistaken or redundant path)    
-#sys.path.remove(os.path.abspath("/usr/local/lib/python3.4/dist-packages/"))
+# Optional: Activate to remove a (mistaken or redundant path)    
 #sys.path.remove(os.path.abspath("/home/christof/Repos/cligs/tmw"))
-#sys.path.remove(os.path.abspath("/usr/lib/python3.4/"))
-#sys.path.remove(os.path.abspath("/usr/local/lib/python3.4/dist-packages/numpy-1.10.4.egg"))
 
-## This is for checking whether the path settings are correct.
+# This is for checking whether the path settings are correct.
 print(sys.path)
